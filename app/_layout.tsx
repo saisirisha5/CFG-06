89a6d300e13c165be8ab6d5a40eb3124af391c3b
unknown <2200030933@kluniversity.in>
2025-06-21 22:28:51 +0530

    msg

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack
+      screenOptions={{ headerShown: false }}
+      initialRouteName="index" // Set index.tsx as the initial route
+    >
+      <Stack.Screen name="index" options={{ title: "Main Page" }} />
+      <Stack.Screen name="Aww/home" options={{ title: "Home Page" }} />
+      <Stack.Screen name="Aww/video" options={{ title: "Video Page" }} />
+      
+    </Stack>
+  );
 }
